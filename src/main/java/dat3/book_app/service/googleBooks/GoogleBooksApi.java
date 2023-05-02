6cb3a041e3c16f3a44407862d0e3b361cef5c658
Kistaf <Jeti544@gmail.com>
2023-05-02 21:31:41 +0200

    first version of pagination impl

--- src/main/java/dat3/book_app/service/googleBooks/GoogleBooksApi.java
+++ src/main/java/dat3/book_app/service/googleBooks/GoogleBooksApi.java
@@ -5,7 +5,9 @@ import dat3.book_app.dto.googleBooks.GoogleBooksAPIResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class GoogleBooksApi implements IGoogleBooksApi {
@@ -28,6 +30,18 @@ public class GoogleBooksApi implements IGoogleBooksApi {
     @Override
     public List<BookResponse> asSearch(String query) {
         var uri = String.format("%s?q='%s'&maxResults=5&filter=paid-ebooks&langRestrict",Uri,query);
+        var response = getRequest(uri,GoogleBooksAPIResponse.class);
+        return response != null ? response.getItems() : new ArrayList<>();
+    }
+
+    @Override
+    public List<BookResponse> paginated(String startIndex) {
+        List<String> uris = Arrays.asList(
+                String.format("%s?q=''&maxResults=15&startIndex=%s&printType=books&langRestrict=en",Uri,startIndex),
+                String.format("%s?q=''&maxResults=15&startIndex=%s&printType=books",Uri,startIndex)
+        );
+        Random random = new Random();
+        String uri = uris.get(random.nextInt(uris.size()));
         var response = getRequest(uri,GoogleBooksAPIResponse.class);
         return response != null ? response.getItems() : new ArrayList<>();
     }
--- src/main/java/dat3/book_app/service/googleBooks/IGoogleBooksApi.java
+++ src/main/java/dat3/book_app/service/googleBooks/IGoogleBooksApi.java
@@ -11,4 +11,6 @@ public interface IGoogleBooksApi {
     List<BookResponse> byAuthor(String author);
 
     List<BookResponse> asSearch(String query);
+
+    List<BookResponse> paginated(String startIndex);
 }
