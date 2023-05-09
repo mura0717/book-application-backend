70f4a5c4b6301181efab771aa677ad12ba964fd5
mura0717 <mura0717@stud.kea.dk>
2023-05-09 22:39:37 +0200

    kaan booklist starts

---
+++ src/main/java/dat3/book_app/service/bookReferences/BookReferencesService.java
@@ -0,0 +1,16 @@
+package dat3.book_app.service.bookReferences;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookReferencesService {
+
+/*
+    public List<BookReferenceResponse> getBooksInBookList(){
+        List<GoogleBook> books = _bookRepository.findAll();
+        return books.stream().map(book -> new BookReferenceResponse(book)).collect(Collectors.toList());
+
+
+        return null;
+    }*/
+}
